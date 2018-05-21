e0228d8c0e2a95a335c2451e551778156348f5b3
fergiendp24 <35159517+fergiendp24@users.noreply.github.com>
2018-05-21 10:59:03 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Animeee</title>
+  </head>
+<body>
+  <style>
+  body{
+    height:100%;
+    background: grey;
+  }
+  footer{
+    height:40px;
+    line-height:10px;
+    width: 98.5%;
+    background:#333;
+    color:#fff;
+
+    position:absolute;
+    bottom:0px;
+  }
+  header{
+    height:130px;
+    line-height:5px;
+    width: 99.7%;
+    background:url(header.jpg);
+    color:#fff;
+
+    position:relative;
+    up:0px;
+  }
+
+  </style>
+  <header>
+    <center>
+      <p><font color="black" size="1pt" >||</font></p>
+      <p><font color="black" size="1pt" >||</font></p>
+      <p><font face="Bradley Hand ITC" size="20pt"><b>ANIMEEE WALLPAPER</b></p></font>
+      <p><font color="black" size="1pt" >||</font></p>
+</center>
+    <center>
+    <h><a href="index.php?home" type="button"><font size="5pt">Home</font></a></h>
+    <h><font color="black">..</font></h>
+    <h><a href="index.php?kategori"><font size="5pt">Kategori</font></a></h>
+    <h><font color="black">..</font></h>
+    <h><a href="masuk_akun.php"><font size="5pt">Akun</font></a></h>
+  </center>
+    </header>
+  <!-- navbar -->
